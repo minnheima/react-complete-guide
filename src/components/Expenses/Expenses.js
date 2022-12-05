35e865891a2e21f1fc19ef292ca8a88b3efad78a
minnheima <hertz.minn@gmail.com>
2022-12-05 13:08:35 +0900

    #74조건부내용출력하기

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,17 +6,31 @@ import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
-  console.log(filteredYear);
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expensesContent = <p>No expenses found</p>;
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} key={expense.id} />
+    ));
+  }
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {props.items.map((expense) => (
-          <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} key={expense.id} />
-        ))}
+        {expensesContent}
+        {/* {filteredExpenses.length === 0 ? (
+          <p>No expenses found</p>
+        ) : (
+          filteredExpenses.map((expense) => (
+            <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} key={expense.id} />
+          ))
+        )} */}
       </Card>
     </div>
   );

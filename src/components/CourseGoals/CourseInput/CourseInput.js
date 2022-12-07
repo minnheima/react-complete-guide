879b0f7d56882ad243fbc211f9a73dea9eb1078a
minnheima <hertz.minn@gmail.com>
2022-12-07 13:51:30 +0900

    #85 Add CSS class dynamically

---
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = (e) => {
+    if (e.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
+    setEnteredValue(e.target.value);
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      //여기서 trim메소드를 호출하는 건 유저들이 공백을 입력했을 경우 배제하기 위해서
+      setIsValid(false);
+      return;
+    }
+
+    props.onAddGoal(enteredValue);
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
+        <label>Course Goal</label>
+        <input
+          type="text"
+          onChange={goalInputChangeHandler}
+        />
+      </div>
+      <Button type="submit">Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
--- src/components/UI/Card.js
+++
@@ -1,8 +0,0 @@
-import "./Card.css";
-
-const Card = (props) => {
-  const classes = `card ${props.className}`;
-  // console.log(`this is card component + ${props.className}`);
-  return <div className={classes}>{props.children}</div>;
-};
-export default Card;

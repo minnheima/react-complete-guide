d633f566f324afb24ba350f46ba6e1f564188da7
minnheima <hertz.minn@gmail.com>
2023-01-18 19:19:08 +0900

    project1-Food order app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import React, { useContext } from "react";
-
-import Login from "./components/Login/Login";
-import Home from "./components/Home/Home";
-import MainHeader from "./components/MainHeader/MainHeader";
-import AuthContext from "./store/auth-context";
+import React, { Fragment } from "react";
+import Header from "../src/components/Layout/Header";
 
 function App() {
-  const ctx = useContext(AuthContext);
   return (
-    <React.Fragment>
-      <MainHeader />
-      <main>
-        {!ctx.isLoggedIn && <Login />}
-        {ctx.isLoggedIn && <Home />}
-      </main>
-    </React.Fragment>
+    <Fragment>
+      <Header />
+    </Fragment>
   );
 }
 

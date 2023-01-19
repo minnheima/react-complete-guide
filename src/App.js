0922a61927002ef2b726469445e885226fd2df51
minnheima <hertz.minn@gmail.com>
2023-01-19 14:51:52 +0900

    #149 React Portal을 통해 modal 추가하기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from "react";
 import Header from "../src/components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 function App() {
   return (
     <Fragment>
+      <Cart /> {/* Modal확인차 잠시 넣어둔 것 */}
       <Header />
       <main>
         <Meals />

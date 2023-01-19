711e42c65875993e3b5fcd5b213b7a7f5f798bd9
minnheima <hertz.minn@gmail.com>
2023-01-19 19:18:42 +0900

    #150 Cart/Modal state management

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Header from "../src/components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
   return (
     <Fragment>
-      <Cart /> {/* Modal확인차 잠시 넣어둔 것 */}
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      {/* Modal확인차 잠시 넣어둔 것 */}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>

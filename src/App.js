cd155b63cf65e8f6f60deff2b822c83551ad2890
minnheima <hertz.minn@gmail.com>
2023-01-30 21:05:01 +0900

    #176 class-based component lifecycle

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
-import React, { useState } from "react";
-
-import Header from "../src/components/Layout/Header";
-import Meals from "./components/Meals/Meals";
-import Cart from "./components/Cart/Cart";
-import CartProvider from "./store/CartProvider";
+import React from "react";
+import UserFinder from "./components/UserFinder";
 
 function App() {
-  const [cartIsShown, setCartIsShown] = useState(false);
-
-  const showCartHandler = () => {
-    setCartIsShown(true);
-  };
-
-  const hideCartHandler = () => {
-    setCartIsShown(false);
-  };
   return (
-    <CartProvider>
-      {cartIsShown && <Cart onClose={hideCartHandler} />}
-      {/* Modal확인차 잠시 넣어둔 것 */}
-      <Header onShowCart={showCartHandler} />
-      <main>
-        <Meals />
-      </main>
-    </CartProvider>
+    <div>
+      <UserFinder />
+    </div>
   );
 }
 

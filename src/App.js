1546b4521587b26ed557da658416e2ed4d1d0799
minnheima <hertz.minn@gmail.com>
2023-02-02 16:03:15 +0900

    #187 async

--- src/App.js
+++ src/App.js
@@ -5,37 +5,21 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  function fetchMoviesHandler() {
-    fetch(`https://swapi.dev/api/films/`)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
-        setMovies(transformedMovies);
-      });
+
+  async function fetchMoviesHandler() {
+    const response = await fetch(`https://swapi.dev/api/films/`);
+    const data = await response.json();
+
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setMovies(transformedMovies);
   }
-  // const dummyMovies = [
-  //   {
-  //     id: 1,
-  //     title: 'Some Dummy Movie',
-  //     openingText: 'This is the opening text of the movie',
-  //     releaseDate: '2021-05-18',
-  //   },
-  //   {
-  //     id: 2,
-  //     title: 'Some Dummy Movie 2',
-  //     openingText: 'This is the second opening text of the movie',
-  //     releaseDate: '2021-05-19',
-  //   },
-  // ];
 
   return (
     <React.Fragment>

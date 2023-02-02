874be87379cb3391b4c0b744dc9cb9448af674f8
minnheima <hertz.minn@gmail.com>
2023-02-03 02:29:00 +0900

    #188 loading state

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMoviesHandler() {
+    setIsLoading(true);
+    setError(null);
     const response = await fetch(`https://swapi.dev/api/films/`);
     const data = await response.json();
 
@@ -19,6 +22,7 @@ function App() {
       };
     });
     setMovies(transformedMovies);
+    setIsLoading(false);
   }
 
   return (
@@ -27,7 +31,8 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading || movies.length > 0 ? <MoviesList movies={movies} /> : <p>Loading...</p>}
+        {!isLoading && movies.length === 0 && <p>Found No Movies.</p>}
       </section>
     </React.Fragment>
   );

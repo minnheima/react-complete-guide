5cdb1a817676adb0a40f2856345f3e129bd20b56
minnheima <hertz.minn@gmail.com>
2023-03-09 19:00:37 +0900

    #256 다중슬라이스작업하기

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,13 @@
 import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const redux = require("redux");
-
-const initialState = {
+const initialCounterState = {
   counter: 0,
   showCounter: true,
 };
 
 const counterSlice = createSlice({
   name: "counter",
-  initialState,
+  initialState: initialCounterState,
   reducers: {
     increment(state) {
       state.counter++;
@@ -25,37 +23,27 @@ const counterSlice = createSlice({
     },
   },
 });
+const initialAuthState = {
+  isAuthenticate: false,
+};
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticate = true;
+    },
+    logout(state) {
+      state.isAuthenticate = false;
+    },
+  },
+});
 
-// const counterReducer = (state = initialState, action) => {
-//   if (action.type === "increment") {
-//     return {
-//       counter: state.counter + 1,
-//       showCounter: state.showCounter,
-//     };
-//   }
-//   if (action.type === "increase") {
-//     return {
-//       counter: state.counter + action.amount,
-//       showCounter: state.showCounter,
-//     };
-//   }
-//   if (action.type === "decrement") {
-//     return {
-//       counter: state.counter - 1,
-//       showCounter: state.showCounter,
-//     };
-//   }
-//   if (action.type === "toggle") {
-//     return {
-//       showCounter: !state.showCounter,
-//       counter: state.counter,
-//     };
-//   }
-//   return state;
-// };
 const store = configureStore({
-  reducer: counterSlice.reducer,
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
+
 export default store;

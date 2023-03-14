1f5647de4d885cf960048b25e7a093128251108e
minnheima <hertz.minn@gmail.com>
2023-03-14 21:32:24 +0900

    #263-264 Recap

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,25 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
 
+const DUMMY_ITEMS = [
+  { id: "p1", title: "test item", price: 10, description: "this is a first item" },
+  { id: "p2", title: "test item2", price: 4, description: "this is a second item" },
+  { id: "p3", title: "test item3", price: 6, description: "this is a third item" },
+];
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_ITEMS.map((item) => (
+          <ProductItem
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            price={item.price}
+            description={item.description}
+          />
+        ))}
       </ul>
     </section>
   );
---
+++ src/store/cart-slice.js
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialCartState = {
+  items: [],
+  totalQuantity: 0,
+};
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: initialCartState,
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id); // 같은 메뉴인것
+      state.totalQuantity++;
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          title: newItem.title,
+          price: newItem.price,
+          totalPrice: newItem.price,
+          quantity: 1,
+        });
+      } else {
+        existingItem.quantity++; // existingItem.quantity = existingItem.quantity + 1;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+      }
+    },
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+      if (existingItem.quantity === 1) {
+        //장바구니 목록에서 완전히 제거해주어야함
+        state.items = state.items.filter((item) => item.id !== id); //  선택한 항목의 id와 같지 않은 것들만 모아서 반환
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
+  },
+});
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
